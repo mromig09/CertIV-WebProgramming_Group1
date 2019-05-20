971e60f742f022f4a50d96c834b4da90c134a342
mromig09 <mromig09@users.noreply.github.com>
2019-05-20 12:27:25 +1000

    todo update

--- term2/week2/index3.js
+++ term2/week2/index3.js
@@ -1,13 +1,41 @@
 var inputField = document.getElementById("inputField");
 var addButton = document.getElementById("addButton");
 var list = document.getElementById("list");
-
+var removeButton = document.getElementById("removeButton");
     addButton.onclick = addItems;
+    removeButton.onclick = deleteAll;
+
 //form.onsubmit
     function addItems(){
-        list.innerHTML += "<ul>" + inputField.value + "</ul>";
-    }
+        var myNewItemId = 10;
 
+        var newItem = document.createElement("li");
+        var newButton = document.createElement("button");
+        newButton.classList.add("delete");
+        //newButton.setAttribute("name", myNewItemId)
+        newButton.appendChild(document.createTextNode("x"));
+        newButton.onclick = myOnClick;
+
+        newItem.setAttribute("id", myNewItemId);
+        newItem.appendChild(document.createTextNode(inputField.value));
+        newItem.appendChild(newButton);
+        list.appendChild(newItem);
+    }
+    
+    function myOnClick(){
+
+        this.parentNode.parentNode.removeChild(this.parentNode);
+        //var elem = document.getElementById('id');
+        //elem.parentNode.removeChild(elem);
+        return false;
+    }    
+
+    function deleteAll(){
+        
+        document.getElementById("list").innerHTML = '';
+    }
+       
+    
 //function addButtonOnClick(){
     //alert("Frank died");
     //var contentToAdd = document.createElement("ul");
