45142c8f6c4d9345548af6469e4e3ac408893ec8
mromig09 <mromig09@users.noreply.github.com>
2019-06-17 10:45:51 +1000

    css

---
+++ term2/Challenge02_6345123/challenge.js
@@ -0,0 +1,9 @@
+var submitButton = document.getElementById("submitButton");
+var inputField = document.getElementById("inputField");
+
+//show results on submit
+submitButton.addEventListener("click", showResults);
+
+function ourButtonClick(){
+    alert("You scored")
+}
